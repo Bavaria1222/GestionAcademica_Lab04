4c4ab175567ac42d8ec85e2a75f2292a972c6605
Bavaria1222 <carlos.alvarez.1222@outlook.com>
2025-06-03 23:13:32 -0600

    Eliminar submódulos y agregar carpetas como parte del repositorio

---
+++ Lab4_Moviles/app/src/main/java/com/example/quiz1/Api/CursoApi.kt
@@ -0,0 +1,19 @@
+package com.example.quiz1.api
+
+import com.example.quiz1.model.Curso
+import retrofit2.Call
+import retrofit2.http.*
+
+interface CursoApi {
+    @GET("cursos")
+    fun listar(): Call<List<Curso>>
+
+    @POST("cursos")
+    fun insertar(@Body curso: Curso): Call<Void>
+
+    @PUT("cursos")
+    fun modificar(@Body curso: Curso): Call<Void>
+
+    @DELETE("cursos/{id}")
+    fun eliminar(@Path("id") id: Int): Call<Void>
+}
