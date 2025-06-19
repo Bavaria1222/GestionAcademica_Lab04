922f598de96d788420aca577f0844dd540bdf636
Bavaria1222 <129691586+Bavaria1222@users.noreply.github.com>
2025-06-19 04:59:42 -0600

    Expand help screen with detailed guide

--- Lab4_Moviles/app/src/main/java/com/example/quiz1/fragment/AyudaFragment.kt
+++ Lab4_Moviles/app/src/main/java/com/example/quiz1/fragment/AyudaFragment.kt
@@ -1,11 +1,9 @@
 package com.example.quiz1.fragment
 
 import android.os.Bundle
-import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.quiz1.R
 
@@ -15,8 +13,6 @@ class AyudaFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_ayuda, container, false)
-        view.findViewById<TextView>(R.id.tvContenidoAyuda).movementMethod = LinkMovementMethod.getInstance()
-        return view
+        return inflater.inflate(R.layout.fragment_ayuda, container, false)
     }
 }
